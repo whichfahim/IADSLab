d2ed9dd6f4f401e6cd10af877734cc378517c06f
whichFahim <31901239+whichfahim@users.noreply.github.com>
2023-10-25 17:07:30 -0500

    Fahim | lab6 | done up to 4
    created navigation to and from home page and about page

--- myappF23/views.py
+++ myappF23/views.py
@@ -1,30 +1,37 @@
-from django.http import HttpResponseNotFound
 from django.http import HttpResponse
 from .models import Category, Course, Student, Instructor
 from django.shortcuts import get_object_or_404
+from django.shortcuts import render
+
+# def index(request):
+#     category_list = Category.objects.all().order_by('id')[:10]
+#     response = HttpResponse()
+#     heading1 = '<h1>' + 'List of categories: ' + '</h1>'
+#     response.write(heading1)
+#     for category in category_list:
+#         para = '<p>'+ str(category) + '</p>'
+#         response.write(para)
+#
+#     course_list = Course.objects.all().order_by('-price')
+#     heading2 = '<h2>' + 'List of courses by price: ' + '</h2>'
+#     response.write(heading2)
+#     for course in course_list:
+#         para = '<p>'+ str(course) + '</p>'
+#         response.write(para)
+#     return response
 
 def index(request):
     category_list = Category.objects.all().order_by('id')[:10]
-    response = HttpResponse()
-    heading1 = '<h1>' + 'List of categories: ' + '</h1>'
-    response.write(heading1)
-    for category in category_list:
-        para = '<p>'+ str(category) + '</p>'
-        response.write(para)
+    return render(request, 'myappF23/index0.html', {'category_list': category_list})
 
-    course_list = Course.objects.all().order_by('-price')
-    heading2 = '<h2>' + 'List of courses by price: ' + '</h2>'
-    response.write(heading2)
-    for course in course_list:
-        para = '<p>'+ str(course) + '</p>'
-        response.write(para)
-    return response
+# def about(request):
+#     response = HttpResponse()
+#     heading = '<h1>' + 'This is a Distance Education Website! Search our Categories to find all available Courses.' + '</h1>'
+#     response.write(heading)
+#     return response
 
 def about(request):
-    response = HttpResponse()
-    heading = '<h1>' + 'This is a Distance Education Website! Search our Categories to find all available Courses.' + '</h1>'
-    response.write(heading)
-    return response
+    return render(request, 'myappF23/about0.html')
 
 def detail(request, category_no):
     response = HttpResponse()
