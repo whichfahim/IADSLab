e12fa80ae4f9a521ac4b3d7263f85d8667f0f5bd
whichFahim <31901239+whichfahim@users.noreply.github.com>
2023-10-24 19:00:22 -0500

    Fahim | lab5 | details page
    Formatted the output and added heading

--- myappF23/views.py
+++ myappF23/views.py
@@ -1,9 +1,7 @@
-from django.shortcuts import render, get_object_or_404
-# Import necessary classes
+from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from .models import Category, Course, Student, Instructor
 
-# Create your views here.
 def index(request):
     category_list = Category.objects.all().order_by('id')[:10]
     response = HttpResponse()
@@ -29,17 +27,37 @@ def about(request):
 
 def detail(request, category_no):
     # Get the category based on the category_no or return a 404 error if it doesn't exist
-    category = get_object_or_404(Category, pk=category_no)
-
+    # category = get_object_or_404(Category, pk=category_no)
+    #
+    # response = HttpResponse()
+    # heading = '<h1' + 'This is the details page' + '</h1>'
+    # response.write(heading)
+    # # Get a list of courses for the specified category
+    # courses = Course.objects.filter(categories_id=category_no)
+    #
+    # for i in courses:
+    #     course = "<li>"+str(i)+"</li>"
+    #     response.write(course)
+    #
+    # return response
+    # category_list = Category.objects.all().order_by('id')[:10]
+    course_list = Course.objects.filter(categories_id=category_no)
     response = HttpResponse()
-    heading = '<h1' + 'This is the details page' + '</h1>'
-    response.write(heading)
-    # Get a list of courses for the specified category
-    courses = Course.objects.filter(categories_id=category_no)
+    # category = Category.objects.filter(id=category_no)
+    category = Category.objects.get(id=1)
+    # print(str(article))
+    heading1 = '<h1>' + 'List of courses with category: ' + str(category) + '</h1>'
+    response.write(heading1)
 
-    for i in courses:
-        course = "<li>"+str(i)+"</li>"
-        response.write(course)
+    for course in course_list:
+        list = '<li>' + str(course) + '</li>'
+        response.write(list)
 
+    # course_list = Course.objects.all().order_by('-price')
+    # heading2 = '<h2>' + 'List of courses by price: ' + '</h2>'
+    # response.write(heading2)
+    # for course in course_list:
+    #     para = '<p>' + str(course) + '</p>'
+    #     response.write(para)
     return response
 
